35d466a80c383a2b97fb7eab0aef1ebf1c870eab
Juliana Thompson <jgtbandeira@gmail.com>
2025-02-09 15:53:52 -0300

    alterando texto e valores

---
+++ script.js
@@ -0,0 +1,16 @@
+function calculoMassaCorporal(p, a) {
+  return p/a*a;
+}
+
+let massaCorporal = calculoMassaCorporal(70, 1.70);
+
+function exibirTexto (tag, texto) {
+  let campo = document.querySelector (tag)
+  campo.innerHTML = texto
+}
+
+exibirTexto("h2", `A sua massa corporal é ${massaCorporal.toFixed(1)}`)
+
+
+
+
